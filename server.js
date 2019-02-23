9222605760085613228261be5215a3839ec96ab9
Ganesh Sarakadam <gsarakad@berkadia.com>
2019-02-23 14:07:11 +0530

    new repo

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-path = require('path');
-const app = express();
-app.use(express.static('./dist/heroku-app'));
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/heroku-app/index.html'));
 
-});
+const app = express();
+app.use(express.static(__dirname + '/dist'));
+
 app.listen(8080, () => {
     console.log('server started');
 
